e5571b1aebf82d0fcb91af037a936c3ecf050653
orangmiliter <42175785+orangmiliter@users.noreply.github.com>
2019-07-10 22:34:36 +0700

    update makaboro

--- makaboro.py
+++ makaboro.py
@@ -1,10 +1,9 @@
-import requests, re, sys, os
-import urllib2
+import requests
 import csv
 import argparse
-from pwn import *
 from bs4 import BeautifulSoup
 from datetime import datetime
+from cookies import cookie
 
 waktu = datetime.now()
 tahun = waktu.year
@@ -12,45 +11,54 @@ bulan = waktu.month
 hari = waktu.day
 #
 parser = argparse.ArgumentParser()
-parser.add_argument("--page", help="Input page target ", nargs='+')
-parser.add_argument("--output", help="Input output name", nargs='+')
+parser.add_argument("--start", help="Input page start ", nargs='+')
+parser.add_argument("--stop", help="input page stop", nargs='+')
+parser.add_argument("--output", help="Output file Name", nargs='+')
 
 args = parser.parse_args()
-argspace = ' '.join(args.page)
-keyword = str(argspace)
-argoutput = ' '.join(args.output)
-keyword2 = str(argoutput)
+startJoin = ''.join(args.start)
+start = str(startJoin)
+stopJoin = ''.join(args.stop)
+stop = str(stopJoin)
+outputJoin = ''.join(args.output)
+output = str(outputJoin)
 
-url = '%s' % keyword
-sess = requests.session()
+start = int(start)
+stop = int(stop)
 
-log.progress("Wait, It's Working...")
-
-#input cookie press f12, find ZHE and PHPSESSID
-myCookie = {
-    'ZHE':'931be96f0472188542f55e28feea8b96',
-    'ZH':'931be96f0472188542f55e28feea8b96',
-    'PHPSESSID':'ldupdnp10tsgo6jdqh84n5mh81'
-}
-
-data = sess.get(url, cookies=myCookie)
-data2 = data.content
-bs = BeautifulSoup(data2, 'lxml')
-table_body = bs.find('table')
-rows = table_body.find_all('tr', class_=None)[1:]
-
-with open('output/zoneH-{}-{}-{}-{}.csv'.format(tahun, bulan, hari, keyword2), 'a') as csvfile:
+with open('output/zoneH-{}-{}-{}-{}.csv'.format(tahun, bulan, hari, output), 'a') as csvfile:
     writecsv = csv.writer(csvfile)
-    writecsv.writerow(["Halaman Terdeface", "Attacker", "Tebas Index", "Kejadian", "Arsip"])
+    writecsv.writerow(["Institusi", "Halaman Terdeface", "Attacker", "Tebas Index", "Kejadian", "Arsip"])
 
-    for row in rows:
-        kolom = row.findAll('td')
+    print("[+] wait, It's Working...")
 
-        if len(kolom) > 1:
-            halaman = kolom[7].text
-            attacker = kolom[1].text
-            tebas = kolom[2].text.replace("H", "Y").replace("", "T").replace("TYT", "Y") #wkwkwkgoblok
-            kejadian = kolom[0].text
-            arsip = kolom[9].find('a').get('href')
-            writecsv.writerow([halaman, attacker, tebas, kejadian, str('www.zone-h.org%s' % arsip)])
-log.info("Done !!")
+    for i in range(start, stop+1):
+        url = 'http://www.zone-h.org/archive/filter=1/special=1/domain=go.id/fulltext=1/page=%s' % i
+        sess = requests.session()
+
+        # log.progress("Wait, It's Working...")
+
+        myCookie = {
+            'ZHE':cookie['ZHE'],
+            'ZH':cookie['ZH'],
+            'PHPSESSID':cookie['PHPSESSID']
+        }
+
+        data = sess.get(url, cookies=myCookie)
+        data2 = data.content
+        bs = BeautifulSoup(data2, 'lxml')
+        table_body = bs.find('table')
+        rows = table_body.find_all('tr', class_=None)[1:]
+
+        for row in rows:
+            kolom = row.findAll('td')
+
+            if len(kolom) > 1:
+                halaman = kolom[7].text
+                attacker = kolom[1].text
+                tebas = kolom[2].text.replace("H", "Y").replace("", "T").replace("TYT", "Y") #wkwkwkgoblok
+                kejadian = kolom[0].text.replace('/', '-')
+                arsip = kolom[9].find('a').get('href')
+                writecsv.writerow([None, halaman, attacker, tebas, str('%s 00:00:00' % kejadian), str('www.zone-h.org%s' % arsip)])
+
+    print("[✔] Done")
